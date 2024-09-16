c45ae74da5c187791ab3f4266d0f952ff924c972
RShnitser <romanshnitser@hotmail.com>
2024-09-15 20:11:17 -0400

    add server

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main(){
-	
+	port := "8080"
+	mux := http.NewServeMux()
+	server := http.Server{
+		Handler: mux,
+		Addr: ":" + port,
+	}
+
+	fmt.Printf("Starting server on port %s\n", port)
+	err := server.ListenAndServe()
+	if err != nil{
+		fmt.Printf("Unable to start server: %v\n", err)
+	}
 }
