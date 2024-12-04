22170271dc3c5f55a2410a6a94efa764829744c8
RShnitser <romanshnitser@hotmail.com>
2024-12-03 22:49:48 -0500

    add handlereadiness

--- main.go
+++ main.go
@@ -5,14 +5,24 @@ import (
 	"net/http"
 )
 
+func handleReadiness(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", " text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
 func main(){
+	filePathRoot := "."
 	port := "8080"
+
 	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot))))
+	mux.HandleFunc("/healthz", handleReadiness)
+	
 	server := http.Server{
 		Handler: mux,
 		Addr: ":" + port,
 	}
-	mux.Handle("/", http.FileServer(http.Dir(".")))
 	fmt.Printf("Starting server on port %s\n", port)
 	err := server.ListenAndServe()
 	if err != nil{
