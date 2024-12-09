c97f6704c6a55ba3fa40370a01f3314c0d43184b
RShnitser <romanshnitser@hotmail.com>
2024-12-08 21:14:39 -0500

    add json decoder

--- main.go
+++ main.go
@@ -26,6 +26,8 @@ func main(){
 	mux.HandleFunc("GET /admin/metrics", cfg.handleWriteHits)
 	mux.HandleFunc("POST /admin/reset", cfg.handleReset)
 	
+	mux.HandleFunc("POST /api/validate_chirp", handleValldatePost)
+	
 	server := http.Server{
 		Handler: mux,
 		Addr: ":" + port,
--- posts.go
+++ posts.go
@@ -1,11 +1,21 @@
 package main
 
 import(
+	"fmt"
 	"net/http"
 )
 
-func handlePosts(w http.ResponseWriter, r *http.Request){
-	type chirp struct {
+func handleValidatePosts(w http.ResponseWriter, r *http.Request){
+	type parameters struct {
         Body string `json:"body"`
+    }
+
+	decoder := json.NewDecoder(r.Body)
+    params := parameters{}
+    err := decoder.Decode(&params)
+    if err != nil {
+       
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf("Error decoding parameters: %s", err)))
     }
 }
