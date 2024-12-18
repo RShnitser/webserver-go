7f86aef0fbc800c32642123a798ffdd58de780a3
RShnitser <romanshnitser@hotmail.com>
2024-12-18 18:25:06 -0500

    create update chirp red query

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -21,3 +21,9 @@ UPDATE users
 SET email = $2, hashed_password = $3, updated_at = NOW()
 WHERE id = $1
 RETURNING *;
+
+-- name: UpdateChirpyRed :one
+UPDATE users 
+SET is_chirpy_red = TRUE
+WHERE id = $1
+RETURNING *;
